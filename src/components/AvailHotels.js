c185ad77e64a8b173fa007fd5a46daba638409ba
IharDorinel <ihardaronin@gmail.com>
2021-08-07 22:21:07 +0300

    deleted commented code

--- src/components/AvailHotels.js
+++ src/components/AvailHotels.js
@@ -29,29 +29,3 @@ export default AvailHotels;
 
 
 
-  // const searchAvail = () => {
-  //   if (data.includes(currentValue)) {
-  //     return (
-  //       <section className="avail-hotels filter-hidden">
-  //         <div className="avail-hotels-container">
-  //           <h1>Available Hotels</h1>
-  //           <div className="avail-hotels-gallery">
-  //             <AvailHotel props={data}/>
-  //           </div>
-  //         </div>
-  //       </section>
-  //     )
-  //   } else {
-  //     return (
-  //       <h1>Sorry, there are no hotels on your request</h1>
-  //     )
-  //   }
-  // }
-  // return null;
-
-
-
-
-
-// data.filter(obj => obj?.country?.includes(currentValue) || obj?.city?.includes(currentValue) ||
-//   obj?.name?.includes(currentValue))
