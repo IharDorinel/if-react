7582ef73d90046a60dbc3ca01ec2d604982e3b61
IharDorinel <ihardaronin@gmail.com>
2021-08-11 17:57:57 +0300

    installed linter plugin, Date Picker, created filter

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -1,135 +1,210 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 // components
-import AvailHotels from "./AvailHotels";
+import AvailHotels from './AvailHotels';
+import Filter from "./Filter";
 
 // images
-import logo_vector from "../styles/booking_images/logo_vector.svg";
-import icon_night from "../styles/booking_images/Night.svg";
-import icon_account from "../styles/booking_images/AccountCircle.svg";
-import kastelmeccano from "../styles/booking_images/kastelmeccano.jpg"
-import google from "../styles/booking_images/google-play-badge.svg";
-import appStore from "../styles/booking_images/App_Store_Badge.svg";
+import logo_vector from '../styles/booking_images/logo_vector.svg';
+import icon_night from '../styles/booking_images/Night.svg';
+import icon_account from '../styles/booking_images/AccountCircle.svg';
+import kastelmeccano from '../styles/booking_images/kastelmeccano.jpg';
+import google from '../styles/booking_images/google-play-badge.svg';
+import appStore from '../styles/booking_images/App_Store_Badge.svg';
 
 
-const TopSection = (props) => {
 
+const TopSection = () => {
 
   const [currentValue, setCurrentValue] = useState('');
 
   const [isVisible, setIsVisible] = useState(false);
 
+  const [filterIsVisible, setFilterIsVisible] = useState(false);
+
+  const [datePickerIsVisible, setDatePickerIsVisible] = useState(false);
+
+  const [checkUpper, setCheckUpper] = useState('check__label--desktop');
+
   const [data, setData] = useState([]);
+
+  const [startDate, setStartDate] = useState(new Date());
+
+  const [endDate, setEndDate] = useState(new Date());
+
+
+
+  const [adultNumber, setAdultNumber] = useState(1);
+
+  const [childrenNumber, setChildrenNumber] = useState(0);
+
+  const [roomNumber, setRoomNumber] = useState(1);
+
+
 
   useEffect(() => {
     fetch('https://fe-student-api.herokuapp.com/api/hotels')
       .then((response) => response.json())
-      .then(data => data.filter(obj => obj?.country?.includes(currentValue) || obj?.city?.includes(currentValue) ||
-        obj?.name?.includes(currentValue)))
-      .then(data => setData(data))
+      .then((data) => data.filter((obj) => obj?.country?.includes(currentValue) || obj?.city?.includes(currentValue)
+        || obj?.name?.includes(currentValue)))
+      .then((data) => setData(data));
   }, [currentValue]);
 
-
-
   const setValue = (event) => {
-   setCurrentValue(event.target.value);
+    setCurrentValue(event.target.value);
   };
 
   const handleSearch = (event) => {
     event.preventDefault();
     setIsVisible(true);
+  };
 
+  const calendarShow = () => {
+    setCheckUpper('check__label--desktop-up');
+    setDatePickerIsVisible(true);
+  }
+
+  const filterShow = () => {
+    setFilterIsVisible((prev) => !prev);
   };
 
 
   return (
     <>
-    <header className="header">
 
+      <header className="header">
 
-      <div className="header-big-container" style={{backgroundImage: {kastelmeccano}}}>
-        <div className="header-big-upper">
-          <a href="#">
-            <img src={logo_vector} className="image"/>
-          </a>
-
-          <nav className="header-nav">
-            <div className="header-nav-words">
-              <a href="#">
-                <span id="stays" className="word">Stays</span>
-              </a>
-              <a href="#">
-                <span id="attractions" className="word">Attractions</span>
-              </a>
-            </div>
-            <div className="header-nav-icons">
-              <a href="#">
-                <img src={icon_night} className="icon-night"/>
-              </a>
-              <a href="#">
-                <img src={icon_account} className="icon-account"/>
-              </a>
-            </div>
-          </nav>
-          <nav className="header-nav-adapt">
+        <div className="header-big-container" style={{ backgroundImage: { kastelmeccano } }}>
+          <div className="header-big-upper">
             <a href="#">
-              <img src={icon_night} className="icon-night icon"/>
+              <img src={logo_vector} className="image" />
             </a>
-            <a href="#">
-              <img src={icon_account} className="icon-account icon"/>
-            </a>
-          </nav>
-        </div>
 
-
-        <p className="search-title">Discover stays to live, work or just relax</p>
-
-      <div className="search-section">
-        <form className="search-form">
-          <div className="search-form__group destination">
-            <svg className="loupe-icon destination__loupe-icon">
-              <use href="#loupe-icon"/>
-            </svg>
-            <input type="text" value={currentValue} name="destination" id="destination" className="destination__input" placeholder=" "
-                   required onChange={setValue}/>
-            <label htmlFor="destination" className="destination__label">Your destination or hotel name</label>
+            <nav className="header-nav">
+              <div className="header-nav-words">
+                <a href="#">
+                  <span id="stays" className="word">Stays</span>
+                </a>
+                <a href="#">
+                  <span id="attractions" className="word">Attractions</span>
+                </a>
+              </div>
+              <div className="header-nav-icons">
+                <a href="#">
+                  <img src={icon_night} className="icon-night" />
+                </a>
+                <a href="#">
+                  <img src={icon_account} className="icon-account" />
+                </a>
+              </div>
+            </nav>
+            <nav className="header-nav-adapt">
+              <a href="#">
+                <img src={icon_night} className="icon-night icon" />
+              </a>
+              <a href="#">
+                <img src={icon_account} className="icon-account icon" />
+              </a>
+            </nav>
           </div>
-          <div className="search-form__group check">
-            <input type="text" className="check__input" name="check-in" id="check-in" placeholder=" "/>
-              <label htmlFor="check-in" className="check__label">Check-in</label>
-              <input type="text" className="check__input" name="check-out" id="check-out" placeholder=" "/>
-                <label htmlFor="check-out" className="check__label">Check-out</label>
-                <label htmlFor="check-in" className="check__label check__label--desktop">Check-in — Check-out</label>
-          </div>
-          <div className="search-form__group related">
-                    <span className="guests_count">
-                    <span id="adults">&nbsp;&nbsp;1 Adult</span>
-                    <span id="children">&nbsp;—&nbsp;0 Children</span>
-                    <span id="rooms">&nbsp;—&nbsp;1 Room</span>
+
+          <p className="search-title">Discover stays to live, work or just relax</p>
+
+          <div className="search-section">
+            <form className="search-form">
+              <div className="search-form__group destination">
+                <svg className="loupe-icon destination__loupe-icon">
+                  <use href="#loupe-icon" />
+                </svg>
+                <input
+                  type="text"
+                  value={currentValue}
+                  name="destination"
+                  id="destination"
+                  className="destination__input"
+                  placeholder=" "
+                  required
+                  onChange={setValue}
+                />
+                <label htmlFor="destination" className="destination__label">Your destination or hotel name</label>
+              </div>
+              <div className="search-form__group check" onClick={calendarShow}>
+
+                { datePickerIsVisible ?
+                  <div>
+                <DatePicker wrapperClassName="datePicker" selected={startDate} startDate={startDate} endDate={endDate} minDate={startDate} onChange={(date) => setStartDate(date)} />
+                <DatePicker wrapperClassName="datePicker" selected={endDate} startDate={startDate} endDate={endDate} minDate={startDate} onChange={(date) => setEndDate(date)} />
+                  </div>
+                  : null }
+
+                <input type="text" className="check__input" name="check-in" id="check-in" placeholder=" " />
+                 <label htmlFor="check-in" className="check__label">Check-in</label>
+                <input type="text" className="check__input" name="check-out" id="check-out" placeholder=" " />
+                 <label htmlFor="check-out" className="check__label">Check-out</label>
+                 <label htmlFor="check-in" className={checkUpper}>Check-in — Check-out</label>
+              </div>
+              <div className="search-form__group related" onClick={filterShow}>
+                <span className="guests_count">
+                  <span id="adults">
+                    {adultNumber}
+                    {' '}
+                    Adults
+                  </span>
+                  <span id="children">
+                    &nbsp;—&nbsp;
+                    {childrenNumber}
+                    {' '}
+                    Children
+                  </span>
+                  <span id="rooms">
+                    &nbsp;—&nbsp;
+                    {roomNumber}
+                    {' '}
+                    Rooms
+                  </span>
                 </span>
 
-          </div>
-          <button type="submit" value="Search" className="submit-button" onClick={handleSearch}>Search</button>
-        </form>
-      </div>
-        <div className="header-mobile-icons">
-          <a href="#">
-            <img
-              className="google-play"
-                 src={google} alt="google-play"/>
-          </a>
-          <a href="#">
-            <img className="app-store"
-                 src={appStore} alt="App_Store"/>
-          </a>
-        </div>
-      </div>
+              </div>
+              <button type="submit" value="Search" className="submit-button" onClick={handleSearch}>Search</button>
 
-    </header>
+
+              {filterIsVisible
+
+                ? ( <Filter adultNumber={adultNumber} setAdultNumber={setAdultNumber}
+                            childrenNumber={childrenNumber} setChildrenNumber={setChildrenNumber}
+                            roomNumber={roomNumber} setRoomNumber={setRoomNumber}/>
+                )
+
+                : null}
+
+            </form>
+          </div>
+
+          <div className="header-mobile-icons">
+            <a href="#">
+              <img
+                className="google-play"
+                src={google}
+                alt="google-play"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="app-store"
+                src={appStore}
+                alt="App_Store"
+              />
+            </a>
+          </div>
+        </div>
+
+      </header>
+
       { isVisible ? <AvailHotels props={data} /> : null}
     </>
-  )
-}
+  );
+};
 
 export default TopSection;
-
