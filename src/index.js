e0095e1b668ad3919adb358e9328e63fa4df62cc
IharDorinel <ihardaronin@gmail.com>
2021-09-22 21:58:14 +0300

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 // components
 import App from './components/App';
@@ -12,7 +13,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,21 @@
 import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['adults', 'children', 'rooms'],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const configureStore = () => {
+  const store = createStore(persistedReducer);
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+};
+
+export const { store, persistor } = configureStore();
